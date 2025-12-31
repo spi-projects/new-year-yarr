e3d06efd7c712a22debbfe53146a6fb29aa061b4
spi-projects <e4946253@gmail.com>
2025-12-31 21:55:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function updateCountdown() {
 
     // test trigger 
 
-    triggerCelebration();
+    //triggerCelebration();
     // Calculate time units
     const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -282,3 +282,4 @@ document.head.appendChild(style);
 
 // ===== Start Application =====
 init();
+
